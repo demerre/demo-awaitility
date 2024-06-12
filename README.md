285174c8ccce67e6aec19f3c822e1d459a830b30
Roberto Méndez <roberto.mendez@lastminute.com>
2024-06-12 07:38:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Awaitility Demo
+
+This project demonstrates how to use Awaitility, a Java library that simplifies testing asynchronous code.
+
+## What is Awaitility?
+
+Awaitility is a Java library that allows you to write tests for asynchronous code in a simple and readable manner. It helps you wait for specific conditions to be met, making it easier to test code that involves waiting or polling for results.
+
+## Download/Dependency
+
+To include Awaitility in your project, add the following dependency to your `pom.xml`:
+
+```xml
+<dependency>
+    <groupId>org.awaitility</groupId>
+    <artifactId>awaitility</artifactId>
+    <version>4.2.0</version>
+    <scope>test</scope>
+</dependency>
---
+++ src/test/java/com/example/AwaitilityTest.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.junit.jupiter.api.Test;
+
+public class AwaitilityTest {
+    private List<String> exampleList = new ArrayList<>();
+    void addToList(String value) {
+        exampleList.add(value);
+    }
+
+    @Test
+    void testAsynchronousOperationWithThreadSleep() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        assertEquals(0, exampleList.size());
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            addToList("Test " + Math.random());
+        });
+        assertEquals(0, exampleList.size());
+        Thread.sleep(2000);
+        assertEquals(1, exampleList.size());
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("Duration with Thread.sleep(): " + timeElapsed);
+    }
+
+    @Test
+    void testAsynchronousOperation() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        assertEquals(0, exampleList.size());
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            addToList("Test " + Math.random());
+        });
+        assertEquals(0, exampleList.size());
+
+        await().atMost(2, SECONDS).until(() -> exampleList.size() == 1);
+
+        assertEquals(1, exampleList.size());
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("Duration with awaitility: " + timeElapsed);
+    }
+}
